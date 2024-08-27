bab1bafbcdbad797eaf3dddabb08db3fdc115390
“lbyul” <rlaquf7737@naver.com>
2024-08-28 04:22:03 +0900

    사이드바: 일정 추가 및 입력 기능 / css 수정

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const deleteConfirmBtn = deleteModal.querySelector('.delete-confirmation-btn');
     const deleteCancelBtn = deleteModal.querySelector('.delete-cancel-btn');
     let itemToDelete = null;
+    const addButton = document.querySelector('.sidebar-schedule-add');
+    const newItem = document.querySelector('.sidebar-new-item');
+    const scheduleInput = newItem.querySelector('.sidebar-schedule-input');
+
 
 
     function showModal(itemText) {
@@ -37,6 +41,52 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 삭제 모달창 취소 버튼
     deleteCancelBtn.addEventListener('click', hideModal);
+
+    
+    // 새 일정 입력 필드 표시
+    addButton.addEventListener('click', function() {
+        newItem.style.display = 'block';
+        scheduleInput.focus();
+        scheduleInput.textContent = '';
+    });
+
+    // 일정 입력 가능하게
+    scheduleInput.setAttribute('contenteditable', 'true');
+
+    // 일정 추가
+    function addNewItem() {
+        const newItemText = scheduleInput.textContent.trim();
+        if (newItemText && newItemText !== '') {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <a href="#">
+                    <div class="sidebar-list-box">
+                        <div class="sidebar-list-cont">
+                            <img src="./src/assets/img/list-circle.svg" alt="" />
+                            <span>${newItemText}</span>
+                        </div>
+                        <div class="sidebar-list-close">
+                            <img src="./src/assets/img/close-btn.svg" alt="" />
+                        </div>
+                    </div>
+                </a>
+            `;
+            scheduleList.appendChild(li);
+        }
+        newItem.style.display = 'none';
+        scheduleInput.textContent = '';
+    }
+
+    // Enter -> 일정 추가 or 줄 변경 막기
+    scheduleInput.addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            addNewItem();
+        }
+    });
+
+    // 포커스를 잃으면 입력 필드 사라지게
+    scheduleInput.addEventListener('blur', addNewItem);
 
 });
 
